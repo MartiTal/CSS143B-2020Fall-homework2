f5d55b97b4fbe05098c3f8bd0e657855ff17bdbd
TalonM <talonm@uw.edu>
2020-10-12 23:01:49 -0700

    Problem 1 complete

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,11 +1,10 @@
 public class Problem1 {
 
     /*
-    In this algorithm, I will switch the number in the current spot of the array with the number in front of the last known location of said number.
+    In this algorithm, I will switch a non-zero number in the current spot of the array with the number in front of the last known location of said number.
     If this number has no last known location, I will switch the current number with the number at the end of the array.
-    An exception to this is that I will switch a 1 with the number before the first 2 in the array provided a 2 has already been encountered.
-    If the number is a 0, I will not touch it.
-    This process will continue until a number does not move, that is to say I would be switching it with the position it is currently in.
+    An exception to this is that I will switch a "1" with the number before the first "2" in the array provided a "2" has already been encountered.
+    This process will continue until a number does not move (or would move backwards), that is to say I would be switching it with the position it either is currently in or behind it.
      */
 
     // Do not change signature (function name, parameters type, and return type)
@@ -13,19 +12,22 @@ public class Problem1 {
         int oneGoesHere = nums.length-1;
         int twoGoesHere = nums.length-1;
         int temp;
-        for (int i = 0; i < nums.length; i++) {
+        int i = 0; //Index value as we pass through the array
+
+        while (i < nums.length) { //I switched to using a while loop, because incrementing at the end of every loop did not work well for this strategy.
             if (nums[i] == 1) {
-                if (i == oneGoesHere) { //Stop when the 1 would be switched with its current location
-                    break;
+                if (i >= oneGoesHere) { //Stop if the 1 would be switched with its current location or a location behind it
+                    return;
                 }
                 temp = nums[i];
                 nums[i] = nums[oneGoesHere];
                 nums[oneGoesHere] = temp;
                 oneGoesHere--; //The next 1 will be placed at the position before where this one exists
 
+
             } else if (nums[i] == 2) {
-                if (i == twoGoesHere) { //Stop when the 2 would be switched with its current location
-                    break;
+                if (i >= twoGoesHere) { //Stop if the 2 would be switched with its current location or a location behind it
+                    return;
                 }
                 temp = nums[i];
                 nums[i] = nums[twoGoesHere];
@@ -33,8 +35,11 @@ public class Problem1 {
                 twoGoesHere--;
                 oneGoesHere = twoGoesHere; //The next non-0 number belongs in this location, before the last 2 in the array.
 
+
+            } else { //If this number is a 0, move to the next number in the array.
+                i++;
             }
-            //If the number at i is a 0, no further action is required.
+
         }
     }
 }
